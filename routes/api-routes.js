74b9f7bf255a070e6d0763b9e49455e6340f0033
jaguar6105 <gilljoseph603@gmail.com>
2020-12-19 17:10:54 -0500

    create api-routes.js

---
+++ routes/api-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+//const Workout = require("../models/workout.js");
+
+//create workout
+router.post("/api/workouts", ({ body }, res) => {
+  
+});
+
+//last workout
+router.get("/api/workouts", (req, res) => {
+  
+});
+
+//add exercise
+router.put("/api/workouts/:id", ({ body }, res) => {
+
+});
+
+//get workouts in range
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+module.exports = router;
