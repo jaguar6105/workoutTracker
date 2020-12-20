f188d2fcb8ff79bd78d6c2eb7ea69b6f97621844
jaguar6105 <gilljoseph603@gmail.com>
2020-12-19 20:10:59 -0500

    add models for workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-//const Workout = require("../models/workout.js");
+const Workout = require("../models/workout.js");
 
 //create workout
 router.post("/api/workouts", ({ body }, res) => {
