a88c088f572800c0fe53541d2467c6a517f487dc
jaguar6105 <gilljoseph603@gmail.com>
2020-12-19 20:46:35 -0500

    add api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,22 +3,56 @@ const Workout = require("../models/workout.js");
 
 //create workout
 router.post("/api/workouts", ({ body }, res) => {
-  
+    Workout.create(body)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 //last workout
 router.get("/api/workouts", (req, res) => {
-  
+    Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 //add exercise
 router.put("/api/workouts/:id", ({ body }, res) => {
+    
+   /* Workout.update(
+        {
+            _id: mongojs.ObjectId(req.params.id)
+        },
+        {
+            $set: {
+                $push: { exercise: body }
+            }
+        },
+        (error, data) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(data);
+            }
+        }
+    );*/
 
 });
 
 //get workouts in range
 router.get("/api/workouts/range", (req, res) => {
-
+    Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 });
 
 module.exports = router;
